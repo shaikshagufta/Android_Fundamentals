0b3c8eec60d27d811bf9d6e0a32ee0d4b24ec2e6
sshagufta3 <shaikshagufta99@gmail.com>
2022-05-24 13:56:54 +0530

    1.2PartA:first interactive UI

--- app/src/main/java/com/example/androidfundamentals/MainActivity.kt
+++ app/src/main/java/com/example/androidfundamentals/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.androidfundamentals
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private var mCount = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    fun showToast(view: View) {
+      Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT).show()
+    }
+    fun countUp(view: View) {
+        mCount++
+        val mShowCount = findViewById<TextView>(R.id.show_count)
+        mShowCount.text = mCount.toString()
     }
 }
